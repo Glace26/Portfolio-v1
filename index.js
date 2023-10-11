39f94c52bc2819ce6678f605916a105b47d3f2de
Glacetredh <71635652+Glace26@users.noreply.github.com>
2023-10-11 15:39:43 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 export { FireflyComponent } from './src/fireflies.js';
+export { MovingText } from './src/moving-text.js';
+export { LinkItem } from './src/link.js';
+export { SkillsComponent } from './src/skills-component.js';
